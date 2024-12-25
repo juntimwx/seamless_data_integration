daa52c9cc40a158a5aafb2865f6a02f5a77175bc
juntimwx <juntima.wnx@gmail.com>
2024-12-25 16:35:31 +0700

    insert data io_projects to database

--- muic_finance/sql/create_master_table.sql
+++ muic_finance/sql/create_master_table.sql
@@ -58,14 +58,14 @@ CREATE TABLE master.master_io_goods (
 )
 
 CREATE TABLE master.mater_io_projects (
-    id INT PRIMARY KEY, -- Unique identifier for the project
+    id BIGINT PRIMARY KEY, -- Unique identifier for the project
     description VARCHAR(MAX),
     cost_ctr_id VARCHAR(50), -- Foreign key reference for cost center => master_cost_center
     ic_strategy_id VARCHAR(100), -- Foreign key reference for IC Strategy  master_ic_strategy
     mu_strategy_id VARCHAR(100), -- Foreign key reference for IC Strategy  master_ic_strategy
-    CONSTRAINT fk_cost_ctr FOREIGN KEY (cost_ctr_id) REFERENCES master_cost_center(id), -- Assuming a table 'master.master_cost_center' with 'id' column
-    CONSTRAINT fk_ic_strategy FOREIGN KEY (ic_strategy_id) REFERENCES master_ic_strategy(id), -- Assuming a table 'master.master_ic_strategy' with id column
-    CONSTRAINT fk_mu_strategy FOREIGN KEY (mu_strategy_id) REFERENCES master_mu_strategy(id),
+    CONSTRAINT fk_cost_ctr FOREIGN KEY (cost_ctr_id) REFERENCES master.master_cost_center(id), -- Assuming a table 'master.master_cost_center' with 'id' column
+    CONSTRAINT fk_ic_strategy FOREIGN KEY (ic_strategy_id) REFERENCES master.master_ic_strategy(id), -- Assuming a table 'master.master_ic_strategy' with id column
+    CONSTRAINT fk_mu_strategy FOREIGN KEY (mu_strategy_id) REFERENCES master.master_mu_strategy(id),
     -- IO_Project	IO_Project_Description	CostCtr	ID_ICST	ID_MUST
 )
 
