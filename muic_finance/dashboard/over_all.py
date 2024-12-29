fd12ab0c95c632d36d4ce004d2df2de20c95bb7b
juntimwx <juntima.wnx@gmail.com>
2024-12-29 14:13:37 +0700

    add line chart

--- muic_finance/dashboard/over_all.py
+++ muic_finance/dashboard/over_all.py
@@ -43,15 +43,11 @@ df_gl = (
 )
 print(df.head())
 
-# df_gl['amount'] = df_gl['amount'].map('{:,.2f}'.format)
 df_gl['amount'] = df_gl['amount'].astype(float)
 
 # create column rank
 df_gl['rank'] = range(1, len(df_gl) + 1)
 total_rows = len(df_gl)
-
-
-print(df_gl.tail())  # ตรวจสอบท้ายตาราง
 
 # create dash app
 app = dash.Dash(
@@ -103,25 +99,84 @@ table_figure =  dash_table.DataTable(
     style_as_list_view=True,
     style_table={
         'minWidth': '100%',
-        'maxHeight': '600px',
+        'maxHeight': '400px',
         'overflowY': 'auto',
         'overflowX': 'auto',
     }
 )
 
+# create chart
+# name month and order month
+month_order = [
+    'October', 'November', 'December', 'January', 'February', 'March', 
+    'April', 'May', 'June', 'July', 'August', 'September'
+]
+month_mapping = {month: index for index, month in enumerate(month_order, start=1)}
+
+# map month with month sort when %m = month month integer such as 1 2 3 ... and %B month name such as January February ...
+df['month_name'] = df['month_sort'].apply(lambda x: pd.to_datetime(str(x), format='%m').strftime('%B'))
+df['custom_month_sort'] = df['month_name'].map(month_mapping)
+
+# preparing data
+df_month_amount = (
+    df.groupby(
+        ['month_name','custom_month_sort','year'],
+        as_index=False
+    )
+    .aggregate({'amount': 'sum'})
+    .sort_values('custom_month_sort', ascending=False)
+)
+
+# line chart
+line_chart = px.line(
+    df_month_amount,
+    x='custom_month_sort',
+    y='amount',
+    color='year',
+    labels={
+        'custom_month_sort': 'Month',
+        'amount': 'Total Amount',
+        'year': 'Year'
+    },
+    title='Cost by month',
+    category_orders={'custom_month_sort': list(month_mapping.values())}
+).update_xaxes(
+    tickmode='array',
+    tickvals=list(month_mapping.values()),
+    ticktext=month_order
+).update_layout(
+    plot_bgcolor='white',
+    title_x=0.5,
+    xaxis=dict(showgrid=False),
+    yaxis=dict(showgrid=True, gridcolor='lightgrey'),
+)
 
 # create app layout
 app.layout = html.Div(
-    className="container mx-2 p-6 bg-slate-200",
+    className="w-full mx-2 p-6 bg-slate-200",
     children=[
         html.H1(
             "ERP 2023",
-            className="text-4xl font-semibold mb-8 text-center text-gray-800"
+            className="text-4xl font-normal mb-8 text-center text-gray-800"
         ),
         html.Div(
-            className="bg-white rounded p-3",
+            className="grid grid-cols-2 gap-4",
             children=[
-                table_figure
+                html.Div(
+                    className="bg-white rounded",
+                    children=[
+                        table_figure
+                    ]
+                ),
+                html.Div(
+                    className="bg-white rounded",
+                    children=[
+                        dcc.Graph(
+                            id='line-chart',
+                            figure=line_chart
+                        )
+                    ]
+                )
             ]
             
         )
