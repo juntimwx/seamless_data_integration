1dd48a3c4d4292fbda805e06030277692d62a57f
juntimwx <juntima.wnx@gmail.com>
2025-01-27 08:29:57 +0700

    final query for student registration

--- muic_student_timeline/sql/final_course_by_student.sql
+++ muic_student_timeline/sql/final_course_by_student.sql
@@ -2,27 +2,36 @@ select std.Code student_id,
 	term.AcademicYear academic_year,
 	term.AcademicTerm academic_term,
 	--curriculumVersion.NameEn version_name,
-	courseGroup.courseGroupGrandParent_name,
-	courseGroup.courseGroupParent_name,
-	courseGroup.couseGroup_name,
+	case when curriculum_structure.courseGroupGrandParent_name is not null then curriculum_structure.courseGroupGrandParent_name
+	    else curriculum_structure.courseGroupParent_name
+	end main_course_group,
+    case when curriculum_structure.courseGroupGrandParent_name is null then curriculum_structure.couseGroup_name
+        --when curriculum_structure.courseGroupParent_name is not null then curriculum_structure.courseGroupParent_name
+        else curriculum_structure.courseGroupParent_name
+    end course_group,
+    case when curriculum_structure.courseGroupGrandParent_name is null then null
+        else curriculum_structure.couseGroup_name
+    end course_track,
+	--curriculum_structure.courseGroupGrandParent_name,
+	--curriculum_structure.courseGroupParent_name,
+	--curriculum_structure.couseGroup_name,
 	course.Code course_code,
-	course.NameEn course_name
+	course.NameEn course_name,
+	course.Credit
 from student.Students std
-inner join student.AdmissionInformations admissionInfo 
+inner join student.AdmissionInformations admissionInfo
 	ON std.Id = admissionInfo.StudentId
-
-left join registration.RegistrationCourses registrationCourse 
+left join registration.RegistrationCourses registrationCourse
 	on std.Id = registrationCourse.StudentId
-left join dbo.Terms term 
+left join dbo.Terms term
 	on registrationCourse.TermId = term.Id
 left join dbo.Courses course
 	on registrationCourse.CourseId = course.Id
-
 inner join curriculum.CurriculumVersions curriculumVersion
 	on admissionInfo.CurriculumVersionId = curriculumVersion.Id
 outer apply (
-	select top 1 
-	courseGroup.NameEn couseGroup_name, 
+	select top 1
+	courseGroup.NameEn couseGroup_name,
 	courseGroupParent.NameEn courseGroupParent_name,
 	courseGroupGrandParent.NameEn courseGroupGrandParent_name
 	from curriculum.CurriculumCourses curriculumCourse
@@ -34,7 +43,8 @@ outer apply (
 		on courseGroupGrandParent.Id = courseGroupParent.CourseGroupId
 	where curriculumCourse.CourseId = registrationCourse.CourseId
 		and courseGroup.CurriculumVersionId = curriculumVersion.Id
-) courseGroup
+) curriculum_structure
 where registrationCourse.Status <> 'd'
 	and std.Code = '6681035'
-order by student_id, academic_year, academic_term, course_code
+--order by student_id, academic_year, academic_term, course_code
+order by student_id, main_course_group, course_group, course_track, academic_year, academic_term, course_code
