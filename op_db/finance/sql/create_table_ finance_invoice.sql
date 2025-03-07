42e50d167f4002d105e7802998c18d46296f9042
juntimwx <juntima.wnx@gmail.com>
2025-03-07 15:55:10 +0700

    update value finance invoice

--- op_db/finance/sql/create_table_ finance_invoice.sql
+++ op_db/finance/sql/create_table_ finance_invoice.sql
@@ -1,4 +1,4 @@
-CREATE TABLE finance_invoice_20250228(
+CREATE TABLE finance_invoice_20250307(
     invoiceId VARCHAR(30) NOT NULL, 
     acaYear VARCHAR(4) NOT NULL, 
     semester INT NOT NULL CHECK(semester BETWEEN 1 AND 4),
