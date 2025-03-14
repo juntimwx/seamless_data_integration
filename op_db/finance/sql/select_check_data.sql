0a58a40baf6c89c649c214c514388f692b735432
juntima.nuc <juntima.nuc@mahidol.edu>
2025-03-14 09:46:35 +0700

    update data finance invoice

---
+++ op_db/finance/sql/select_check_data.sql
@@ -0,0 +1,9 @@
+select distinct * from finance_invoice_20250314
+-- select distinct * from finance_invoice_20250307
+-- select distinct * from finance_invoice_20250228
+-- select distinct * from finance_invoice_20250220
+-- select distinct * from finance_invoice_20250214
+-- select distinct * from finance_invoice_20250131
+-- select distinct * from finance_invoice_20250124
+-- select distinct * from finance_invoice_20250118
+-- select distinct * from finance_invoice_20250106
